2027878c98463750d49e0402d9bff806b7917329
Ronald Bett <rbett@jambopay.com>
2021-11-05 09:03:50 +0300

    Initial commit

---
+++ DynamicPrograming/NthFibonacci.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicPrograming
+{
+    public class NthFibonacci
+    {
+        private static Dictionary<int, long> memo = new();
+
+        public static int NthFib(int n)
+        {
+            if (n <= 2) return 1;
+
+            return NthFib(n - 1) + NthFib(n - 2);
+        }
+
+        public static long NthFibMemo(int n)
+        {
+            if (memo.ContainsKey(n)) return memo[n];
+
+            if (n <= 2) return 1;
+
+            var res = NthFibMemo(n - 1) + NthFibMemo(n - 2);
+            memo.Add(n,res);
+
+            return memo[n];
+        }
+
+        public static long NthFibDyn(int n)
+        {
+            int res=0;
+            
+            if (n <= 2) return 1;
+            int dp1 = 1;
+            int dp2 = 1;
+            for (int i = 3; i <=n; i++)
+            {
+                res = dp1 + dp2;
+                dp1 = dp2;
+                dp2 = res;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ DynamicPrograming/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DynamicPrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //await Task.Run(() => );
+            Console.WriteLine($"{NthFibonacci.NthFibDyn(50)}");
+            //Console.WriteLine($"Memoized => {NthFibonacci.NthFibMemo(45)}");
+        }
+    }
+}
